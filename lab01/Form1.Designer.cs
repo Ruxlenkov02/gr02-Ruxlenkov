46ad6f291a20cad7743d3fa81cf9468a5eac114a
Ruxlenkov02 <arabfishfamily@gmail.com>
2021-12-22 16:40:21 +0000

    добавление файлов

---
+++ lab01/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace lab01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_Figure = new System.Windows.Forms.ComboBox();
+            this.label_figure = new System.Windows.Forms.Label();
+            this.button_craft = new System.Windows.Forms.Button();
+            this.button_DELETE = new System.Windows.Forms.Button();
+            this.picture_craft = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_craft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox_Figure
+            // 
+            this.comboBox_Figure.FormattingEnabled = true;
+            this.comboBox_Figure.Items.AddRange(new object[] {
+            "square",
+            "circle"});
+            this.comboBox_Figure.Location = new System.Drawing.Point(87, 16);
+            this.comboBox_Figure.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox_Figure.Name = "comboBox_Figure";
+            this.comboBox_Figure.Size = new System.Drawing.Size(82, 21);
+            this.comboBox_Figure.TabIndex = 0;
+            // 
+            // label_figure
+            // 
+            this.label_figure.AutoSize = true;
+            this.label_figure.Location = new System.Drawing.Point(11, 18);
+            this.label_figure.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_figure.Name = "label_figure";
+            this.label_figure.Size = new System.Drawing.Size(36, 13);
+            this.label_figure.TabIndex = 1;
+            this.label_figure.Text = "Figure";
+            // 
+            // button_craft
+            // 
+            this.button_craft.Location = new System.Drawing.Point(193, 18);
+            this.button_craft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_craft.Name = "button_craft";
+            this.button_craft.Size = new System.Drawing.Size(61, 22);
+            this.button_craft.TabIndex = 2;
+            this.button_craft.Text = "ADD";
+            this.button_craft.UseVisualStyleBackColor = true;
+            this.button_craft.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_DELETE
+            // 
+            this.button_DELETE.Location = new System.Drawing.Point(276, 18);
+            this.button_DELETE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_DELETE.Name = "button_DELETE";
+            this.button_DELETE.Size = new System.Drawing.Size(70, 19);
+            this.button_DELETE.TabIndex = 3;
+            this.button_DELETE.Text = "DELETE";
+            this.button_DELETE.UseVisualStyleBackColor = true;
+            this.button_DELETE.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // picture_craft
+            // 
+            this.picture_craft.Location = new System.Drawing.Point(9, 44);
+            this.picture_craft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picture_craft.Name = "picture_craft";
+            this.picture_craft.Size = new System.Drawing.Size(506, 244);
+            this.picture_craft.TabIndex = 4;
+            this.picture_craft.TabStop = false;
+            this.picture_craft.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.picture_craft.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.picture_craft.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.picture_craft);
+            this.Controls.Add(this.button_DELETE);
+            this.Controls.Add(this.button_craft);
+            this.Controls.Add(this.label_figure);
+            this.Controls.Add(this.comboBox_Figure);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Lab01_Ruhlenkov";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture_craft)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_Figure;
+        private System.Windows.Forms.Label label_figure;
+        private System.Windows.Forms.Button button_craft;
+        private System.Windows.Forms.Button button_DELETE;
+        private System.Windows.Forms.PictureBox picture_craft;
+    }
+}
+
---
+++ lab01/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab01
+{
+    public partial class Form1 : Form
+    {
+        int old_x, old_y;
+        List<Figure> lst = new List<Figure>();
+        Figure createFigure(string fig_type)
+        {
+            switch (fig_type)
+            {
+                case "circle": return new Circle();
+                case "square": return new Square();
+            }
+            return null;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.White, 0, 0,picture_craft.Width, picture_craft.Height);
+            foreach (Figure fig in lst)
+                fig.draw(e.Graphics);
+        }
+        void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            foreach (Figure fig in lst)
+                fig.selected = false;
+            for (int i = lst.Count - 1; i >= 0; i--)
+            {
+                Figure fig = lst[i];
+                fig.selected |= fig.test(e.X, e.Y);
+                if (fig.selected == true) break;
+            }
+            picture_craft.Invalidate();
+        }
+
+         void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int dx = e.X - old_x;
+                int dy = e.Y - old_y;
+                foreach (Figure fig in lst)
+                {
+                    if (fig.selected == false) continue;
+                    fig.pos_x += dx;
+                    fig.pos_y += dy;
+
+                }
+                picture_craft.Invalidate();
+
+            }
+            old_x = e.X;
+            old_y = e.Y;
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            Figure fig = createFigure(comboBox_Figure.Text);
+            if (fig == null) return;
+            fig.pos_x = 100.0f;
+            fig.pos_y = 100.0f;
+            lst.Add(fig);
+            picture_craft.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void button2_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while(i<lst.Count)
+            {
+                if (lst[i].selected == true) lst.RemoveAt(i);
+                i++;
+            }
+            picture_craft.Invalidate();
+        }
+
+      
+
+       
+    }
+}
